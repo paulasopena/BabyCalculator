1b5bf36aefcc528d48d36dba42096ac3a7577718
paulasopena <paulasopenacoello@gmail.com>
2023-01-02 21:16:00 +0100

    Tan, cos, sin functionalities

--- app/src/main/java/edu/upc/dsa/babycalculator/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/babycalculator/MainActivity.java
@@ -13,82 +13,132 @@ import edu.upc.dsa.babycalculator.values.Operation;
 
 public class MainActivity extends AppCompatActivity {
     String numbers;
+    String numbersShown;
     Operation operation;
-    Stack<Integer> results;
+    int degreesOrRadians=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         numbers="";
-        results=new Stack<Integer>();
+        numbersShown="";
         setLabel("");
     }
     public void Clear(View view){
         numbers="";
+        numbersShown="";
         setLabel("");
     }
     public void Zero(View view) {
         numbers=numbers+"0";
-        setLabel(numbers);
+        numbersShown=numbersShown+"0";
+        setLabel(numbersShown);
     }
     public void One(View view) {
         numbers=numbers+"1";
-        setLabel(numbers);
+        numbersShown=numbersShown+"1";
+        setLabel(numbersShown);
     }
     public void Two(View view) {
         numbers=numbers+"2";
-        setLabel(numbers);
+        numbersShown=numbersShown+"2";
+        setLabel(numbersShown);
     }
     public void Three(View view) {
         numbers=numbers+"3";
-        setLabel(numbers);
+        numbersShown=numbersShown+"3";
+        setLabel(numbersShown);
     }
     public void Four(View view) {
         numbers=numbers+"4";
-        setLabel(numbers);
+        numbersShown=numbersShown+"4";
+        setLabel(numbersShown);
     }
     public void Five(View view) {
         numbers=numbers+"5";
-        setLabel(numbers);
+        numbersShown=numbersShown+"5";
+        setLabel(numbersShown);
     }
     public void Six(View view) {
         numbers=numbers+"6";
-        setLabel(numbers);
+        numbersShown=numbersShown+"6";
+        setLabel(numbersShown);
     }
     public void Seven(View view) {
         numbers=numbers+"7";
-        setLabel(numbers);
+        numbersShown=numbersShown+"7";
+        setLabel(numbersShown);
     }
     public void Eight(View view) {
         numbers=numbers+"8";
-        setLabel(numbers);
+        numbersShown=numbersShown+"8";
+        setLabel(numbersShown);
     }
     public void Nine(View view) {
         numbers=numbers+"9";
-        setLabel(numbers);
+        numbersShown=numbersShown+"9";
+        setLabel(numbersShown);
     }
     public void Minus(View view) {
         numbers=numbers+" ";
+        numbersShown=numbersShown+"-";
         this.operation=new Operation("-");
-        setLabel("");
+        setLabel(numbersShown);
     }
     public void Multiply(View view) {
         numbers=numbers+" ";
+        numbersShown=numbersShown+"x";
         this.operation=new Operation("x");
-        setLabel("");
+        setLabel(numbersShown);
     }
     public void Sum(View view) {
         numbers=numbers+" ";
+        numbersShown=numbersShown+"+";
         this.operation=new Operation("+");
-        setLabel("");
+        setLabel(numbersShown);
     }
     public void Divide(View view) {
         numbers=numbers+" ";
+        numbersShown=numbersShown+"÷";
         this.operation=new Operation("÷");
-        setLabel("");
+        setLabel(numbersShown);
     }
-    public int Equal(View view){
+    public void Sinus(View view){
+        numbers=numbers+" ";
+        numbersShown=numbersShown+"sin";
+        this.operation=new Operation("sin");
+        setLabel(numbersShown);
+    }
+    public void Cosinus(View view){
+        numbers=numbers+" ";
+        numbersShown=numbersShown+"cos";
+        this.operation=new Operation("cos");
+        setLabel(numbersShown);
+    }
+    public void Tangent(View view){
+        numbers=numbers+" ";
+        numbersShown=numbersShown+"tan";
+        this.operation=new Operation("tan");
+        setLabel(numbersShown);
+    }
+    public void Comma(View view){
+        numbers=numbers+".";
+        numbersShown=numbersShown+".";
+        setLabel(numbersShown);
+    }
+    public void Pi(View view) {
+        numbers=numbers+ Math.PI;
+        numbersShown=numbersShown+Math.PI;
+        setLabel(numbersShown);
+    }
+    public void setDegrees(View view){
+        this.degreesOrRadians=0;
+    }
+    public void setRadians(View view){
+        this.degreesOrRadians=1;
+    }
+    public double Equal(View view){
         String factor1="0";
         String factor2="0";
         for(int i =0; i<numbers.length();i++){
@@ -97,13 +147,21 @@ public class MainActivity extends AppCompatActivity {
                 factor1=numbers.substring(0,i);
                 Log.i("FACTOR1:","FACTOR1="+factor1);
                 factor2=numbers.substring(i+1);
-                Log.i("FACTOR1:","FACTOR1="+factor2);
+                Log.i("FACTOR1:","FACTOR2="+factor2);
                 break;
             }
         }
         this.numbers="";
-        int result=this.operation.computeOperation(Integer.parseInt(factor1),Integer.parseInt(factor2));
-        results.add(result);
+        double result;
+        if(this.operation.getIdOperation().length()!=1){
+            result=this.operation.computeOperation(0,Double.parseDouble(factor2),this.degreesOrRadians);
+            setLabel(String.valueOf(result));
+            if(this.degreesOrRadians==0){
+                return (result*360/(2*Math.PI));
+            }
+            return result;
+        }
+        result=this.operation.computeOperation(Double.parseDouble(factor1),Double.parseDouble(factor2),this.degreesOrRadians);
         setLabel(String.valueOf(result));
         return result;
     }
@@ -113,22 +171,6 @@ public class MainActivity extends AppCompatActivity {
         EditText editorTitle = (EditText) findViewById (R.id.Edit_Title_Update);
         editorTitle.setText(updateTitle);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- app/src/main/java/edu/upc/dsa/babycalculator/values/Operation.java
+++ app/src/main/java/edu/upc/dsa/babycalculator/values/Operation.java
@@ -11,7 +11,10 @@ public class Operation {
     public Operation(String idOperation){
         this.idOperation=idOperation;
     }
-    public int computeOperation(int factor1,int factor2){
+    public double computeOperation(double factor1,double factor2,int degreesOrRadians){
+        if ((idOperation.length()!=1)&&(degreesOrRadians==0)){
+            factor2=factor2*2*Math.PI/360;
+        }
         if(Objects.equals(idOperation, "+")){
             return (factor1+factor2);
         }
@@ -24,7 +27,19 @@ public class Operation {
         if (Objects.equals(idOperation, "x")){
             return (factor1*factor2);
         }
+        if(Objects.equals(idOperation, "sin")){
+            return Math.sin(factor2);
+        }
+        if(Objects.equals(idOperation, "cos")){
+            return Math.cos(factor2);
+        }
+        if(Objects.equals(idOperation,"tan")){
+            return Math.tan(factor2);
+        }
         return -1;
+    }
+    public String getIdOperation(){
+        return this.idOperation;
     }
 
 }
